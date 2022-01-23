11dd7a731c33018e26be7608b204aedeb515833f
goker <gokerakce8@gmail.com>
2022-01-23 17:54:18 +0000

    Added aspnetcore identity

---
+++ src/AuthServer.App/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthServer.App.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    public string? Title { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    [ProtectedPersonalData]
+    public DateTime? DateOfBirth { get; set; }
+}
--- src/AuthServer.App/Program.cs
+++ src/AuthServer.App/Program.cs
@@ -1,10 +1,53 @@
+using AuthServer.App.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//
+// App settings configuration 
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{environment}.json", true)
+    .AddEnvironmentVariables();
+
+if (string.IsNullOrEmpty(environment) || environment.ToLower() == "development")
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
+//
+// Web host configuration
+var isDevelopment = string.IsNullOrEmpty(environment) || environment.ToLower() == "development";
+builder.WebHost.UseKestrel(opt =>
+    {
+        if (isDevelopment == false)
+        {
+            opt.ListenAnyIP(80);
+        }
+    })
+    .UseConfiguration(builder.Configuration)
+    .UseDefaultServiceProvider((context, options) =>
+    {
+        options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+    });
+
+//
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+//
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
